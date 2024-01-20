a23918d93351126f078ca30006f5d887bebc14fe
Mohit Agrawal <mohitagrawal1305@gmail.com>
2024-01-20 18:55:33 +0530

    init

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center p-24">
+    <main className="home-page flex h-screen flex-col items-center justify-center p-24">
       Comming Soon
     </main>
   );
